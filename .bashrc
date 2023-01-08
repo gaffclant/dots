33672a42bc15a67cc106fdb92f1ec9c0ce6f692b
Gaffclant <gaffclant@gmail.com>
2023-01-07 18:16:10 -0600

    Light Theme

--- .bashrc
+++ .bashrc
@@ -8,6 +8,7 @@
 [[ $PS1 && -f /usr/share/bash-completion/bash_completion ]] && \
     . /usr/share/bash-completion/bash_completion
 
+alias ride='/home/gaffclant/github/ride/_/ride45/Ride-4.5-linux-x64/Ride-4.5'
 alias dyapl='sudo docker run -e RIDE_INIT=serve:*:4502 -p 4502:4502 dyalog/dyalog'
 alias rm='rm -i'
 alias vim='nvim'
@@ -29,6 +30,6 @@ function cd {
 PS1="\[\033[1;35m\]\w \[\033[1;36m\]\[\033[0;0m\] "
 PATH=/home/gaffclant/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl
 PATH=/home/gaffclant/bin:/home/gaffclant/bin:/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/lib/jvm/default/bin:/usr/bin/site_perl:/usr/bin/vendor_perl:/usr/bin/core_perl:/home/gaffclant/.local/bin:/usr/share/fonts/:/home/gaffclant/.local/share/gem/ruby/3.0.0/bin:$HOME/.cargo/env
-# tulizu show /home/gaffclant/tizu/startup.issue
+
 test -r "/home/gaffclant/.dir_colors" && eval $(dircolors /home/gaffclant/.dir_colors)
 . "$HOME/.cargo/env"
--- .config/dwm-6.3/dwm.c
+++ .config/dwm-6.3/dwm.c
@@ -178,6 +178,7 @@ static void destroynotify(XEvent *e);
 static void detach(Client *c);
 static void detachstack(Client *c);
 static Monitor *dirtomon(int dir);
+static Monitor *numtomon(int num);
 static void drawbar(Monitor *m);
 static void drawbars(void);
 static void enternotify(XEvent *e);
@@ -185,6 +186,7 @@ static void expose(XEvent *e);
 static void focus(Client *c);
 static void focusin(XEvent *e);
 static void focusmon(const Arg *arg);
+static void focusnthmon(const Arg *arg);
 static void focusstack(const Arg *arg);
 static void hideborder(const Arg *arg);
 static Atom getatomprop(Client *c, Atom prop);
@@ -229,6 +231,7 @@ static void sigchld(int unused);
 static void spawn(const Arg *arg);
 static void tag(const Arg *arg);
 static void tagmon(const Arg *arg);
+static void tagnthmon(const Arg *arg);
 static void tile(Monitor *);
 static void togglebar(const Arg *arg);
 static void togglefloating(const Arg *arg);
@@ -718,6 +721,18 @@ dirtomon(int dir)
 	return m;
 }
 
+Monitor *
+numtomon(int num)
+{
+	Monitor *m = NULL;
+	int i = 0;
+
+	for(m = mons, i=0; m->next && i < num; m = m->next){
+		i++;
+	}
+	return m;
+}
+
 void
 drawbar(Monitor *m)
 {
@@ -855,6 +870,21 @@ focusmon(const Arg *arg)
 	if (!mons->next)
 		return;
 	if ((m = dirtomon(arg->i)) == selmon)
+		return;
+	unfocus(selmon->sel, 0);
+	selmon = m;
+	focus(NULL);
+}
+
+void
+focusnthmon(const Arg *arg)
+{
+	Monitor *m;
+
+	if (!mons->next)
+		return;
+
+	if ((m = numtomon(arg->i)) == selmon)
 		return;
 	unfocus(selmon->sel, 0);
 	selmon = m;
@@ -1751,6 +1781,14 @@ tagmon(const Arg *arg)
 	if (!selmon->sel || !mons->next)
 		return;
 	sendmon(selmon->sel, dirtomon(arg->i));
+}
+
+void
+tagnthmon(const Arg *arg)
+{
+	if (!selmon->sel || !mons->next)
+		return;
+	sendmon(selmon->sel, numtomon(arg->i));
 }
 
 void
